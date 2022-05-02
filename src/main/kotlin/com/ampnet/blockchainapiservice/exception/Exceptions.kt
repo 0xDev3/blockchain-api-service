cbc4655c1350bedc3568e999d424e0fbc85781b3
Domagoj Latečki <domagoj.latecki@gmail.com>
2022-05-02 14:17:27 +0200

    SD-770: add ERC20 balance request table and remove old code

--- src/main/kotlin/com/ampnet/blockchainapiservice/exception/Exceptions.kt
+++ src/main/kotlin/com/ampnet/blockchainapiservice/exception/Exceptions.kt
@@ -2,7 +2,6 @@ package com.ampnet.blockchainapiservice.exception
 
 import com.ampnet.blockchainapiservice.util.ChainId
 import org.springframework.http.HttpStatus
-import java.util.UUID
 
 abstract class ServiceException(
     val errorCode: ErrorCode,
@@ -31,26 +30,6 @@ class UnsupportedChainIdException(chainId: ChainId) : ServiceException(
 ) {
     companion object {
         private const val serialVersionUID: Long = -8803854722161717146L
-    }
-}
-
-class ExpiredValidationMessageException(messageId: UUID) : ServiceException(
-    errorCode = ErrorCode.VALIDATION_MESSAGE_EXPIRED,
-    httpStatus = HttpStatus.BAD_REQUEST,
-    message = "Validation message with ID: $messageId has expired"
-) {
-    companion object {
-        private const val serialVersionUID: Long = -8531346107518192369L
-    }
-}
-
-class BadSignatureException(message: String) : ServiceException(
-    errorCode = ErrorCode.BAD_SIGNATURE,
-    httpStatus = HttpStatus.BAD_REQUEST,
-    message = message
-) {
-    companion object {
-        private const val serialVersionUID: Long = 1135423205237947970L
     }
 }
 
--- src/test/kotlin/com/ampnet/blockchainapiservice/TestData.kt
+++ src/test/kotlin/com/ampnet/blockchainapiservice/TestData.kt
@@ -1,6 +1,5 @@
 package com.ampnet.blockchainapiservice
 
-import com.ampnet.blockchainapiservice.model.result.UnsignedVerificationMessage
 import com.ampnet.blockchainapiservice.util.UtcDateTime
 import com.ampnet.blockchainapiservice.util.WalletAddress
 import java.time.Duration
@@ -9,21 +8,22 @@ import java.util.UUID
 
 object TestData {
 
-    val UNSIGNED_MESSAGE = UnsignedVerificationMessage(
-        id = UUID.fromString("7d86b0ac-a9a6-40fc-ac6d-2a29ca687f73"),
-        walletAddress = WalletAddress("0x865f603F42ca1231e5B5F90e15663b0FE19F0b21"),
-        createdAt = UtcDateTime(OffsetDateTime.parse("2022-01-01T00:00:00Z")),
-        validUntil = UtcDateTime(OffsetDateTime.parse("2022-01-01T02:00:00Z"))
-    )
+    // TODO SD-771
+//    val UNSIGNED_MESSAGE = UnsignedVerificationMessage(
+//        id = UUID.fromString("7d86b0ac-a9a6-40fc-ac6d-2a29ca687f73"),
+//        walletAddress = WalletAddress("0x865f603F42ca1231e5B5F90e15663b0FE19F0b21"),
+//        createdAt = UtcDateTime(OffsetDateTime.parse("2022-01-01T00:00:00Z")),
+//        validUntil = UtcDateTime(OffsetDateTime.parse("2022-01-01T02:00:00Z"))
+//    )
 
     // this message was signed using Metamask
-    val SIGNED_MESSAGE = UNSIGNED_MESSAGE.toSignedMessage(
-        signature = "0x2601a91eed301102ca423ffc36e43b4dc096bb556ecfb83f508047b34ab7236f4cd1eaaae98ee8eac9cde62988f062" +
-            "891f3c84e241d320cf338bdfc17a51bc131b",
-        messageId = UUID.randomUUID(),
-        now = UtcDateTime(OffsetDateTime.parse("2022-01-01T01:00:00Z")),
-        validityDuration = Duration.ofHours(1L)
-    )
+//    val SIGNED_MESSAGE = UNSIGNED_MESSAGE.toSignedMessage(
+//        signature = "0x2601a91eed301102ca423ffc36e43b4dc096bb556ecfb83f508047b34ab7236f4cd1eaaae98ee8eac9cde62988f062" +
+//            "891f3c84e241d320cf338bdfc17a51bc131b",
+//        messageId = UUID.randomUUID(),
+//        now = UtcDateTime(OffsetDateTime.parse("2022-01-01T01:00:00Z")),
+//        validityDuration = Duration.ofHours(1L)
+//    )
 
     const val TOO_SHORT_SIGNATURE = "0x"
     const val INVALID_SIGNATURE = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" +
