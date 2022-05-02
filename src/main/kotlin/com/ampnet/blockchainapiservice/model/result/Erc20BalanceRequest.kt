465af39fdb71a9eb5c60af080bdf1eaafc6307f1
Domagoj Latečki <domagoj.latecki@gmail.com>
2022-05-02 15:08:57 +0200

    SD-770: implement ERC20 balance request repository

---
+++ src/main/kotlin/com/ampnet/blockchainapiservice/model/result/Erc20BalanceRequest.kt
@@ -0,0 +1,23 @@
+package com.ampnet.blockchainapiservice.model.result
+
+import com.ampnet.blockchainapiservice.model.SendScreenConfig
+import com.ampnet.blockchainapiservice.util.BlockNumber
+import com.ampnet.blockchainapiservice.util.ChainId
+import com.ampnet.blockchainapiservice.util.ContractAddress
+import com.ampnet.blockchainapiservice.util.SignedMessage
+import com.ampnet.blockchainapiservice.util.WalletAddress
+import com.fasterxml.jackson.databind.JsonNode
+import java.util.UUID
+
+data class Erc20BalanceRequest(
+    val id: UUID,
+    val chainId: ChainId,
+    val redirectUrl: String,
+    val tokenAddress: ContractAddress,
+    val blockNumber: BlockNumber?,
+    val requestedWalletAddress: WalletAddress?,
+    val actualWalletAddress: WalletAddress?,
+    val signedMessage: SignedMessage?,
+    val arbitraryData: JsonNode?,
+    val sendScreenConfig: SendScreenConfig
+)
---
+++ src/main/kotlin/com/ampnet/blockchainapiservice/repository/Erc20BalanceRequestRepository.kt
@@ -0,0 +1,13 @@
+package com.ampnet.blockchainapiservice.repository
+
+import com.ampnet.blockchainapiservice.model.params.StoreErc20BalanceRequestParams
+import com.ampnet.blockchainapiservice.model.result.Erc20BalanceRequest
+import com.ampnet.blockchainapiservice.util.SignedMessage
+import com.ampnet.blockchainapiservice.util.WalletAddress
+import java.util.UUID
+
+interface Erc20BalanceRequestRepository {
+    fun store(params: StoreErc20BalanceRequestParams): Erc20BalanceRequest
+    fun getById(id: UUID): Erc20BalanceRequest?
+    fun setSignedMessage(id: UUID, walletAddress: WalletAddress, signedMessage: SignedMessage): Boolean
+}
