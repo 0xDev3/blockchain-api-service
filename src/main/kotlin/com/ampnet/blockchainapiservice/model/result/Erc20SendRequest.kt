f3d1511589c66c383b7cfd4ba505f75243b32ae8
Domagoj Latečki <domagoj.latecki@gmail.com>
2022-05-26 10:42:01 +0200

    SD-791: refactor FullErc20SendRequest

--- src/main/kotlin/com/ampnet/blockchainapiservice/model/result/Erc20SendRequest.kt
+++ src/main/kotlin/com/ampnet/blockchainapiservice/model/result/Erc20SendRequest.kt
@@ -4,8 +4,12 @@ import com.ampnet.blockchainapiservice.model.ScreenConfig
 import com.ampnet.blockchainapiservice.util.Balance
 import com.ampnet.blockchainapiservice.util.ChainId
 import com.ampnet.blockchainapiservice.util.ContractAddress
+import com.ampnet.blockchainapiservice.util.FunctionData
+import com.ampnet.blockchainapiservice.util.Status
+import com.ampnet.blockchainapiservice.util.TransactionData
 import com.ampnet.blockchainapiservice.util.TransactionHash
 import com.ampnet.blockchainapiservice.util.WalletAddress
+import com.ampnet.blockchainapiservice.util.WithTransactionData
 import com.fasterxml.jackson.databind.JsonNode
 import java.util.UUID
 
@@ -20,4 +24,21 @@ data class Erc20SendRequest(
     val txHash: TransactionHash?,
     val arbitraryData: JsonNode?,
     val screenConfig: ScreenConfig
-)
+) {
+    fun withTransactionData(
+        status: Status,
+        data: FunctionData,
+        transactionInfo: BlockchainTransactionInfo?
+    ): WithTransactionData<Erc20SendRequest> =
+        WithTransactionData(
+            value = this,
+            status = status,
+            transactionData = TransactionData(
+                txHash = this.txHash,
+                fromAddress = transactionInfo?.from,
+                toAddress = transactionInfo?.to?.toContractAddress() ?: this.tokenAddress,
+                data = data,
+                blockConfirmations = transactionInfo?.blockConfirmations
+            )
+        )
+}
