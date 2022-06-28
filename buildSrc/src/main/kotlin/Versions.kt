cb0a222ce638e5918c770126b79922363df42ff0
Domagoj Latečki <domagoj.latecki@gmail.com>
2022-06-28 13:46:45 +0200

    SD-881: bump version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 
 object Versions {
 
-    const val project = "0.7.0"
+    const val project = "0.8.0"
 
     object Compile {
         const val kotlin = "1.6.10"
