c01d87a61e1eabdd687c42497e9a3fee2cdd4b32
Domagoj Latečki <domagoj.latecki@gmail.com>
2022-07-25 13:17:14 +0200

    SD-904: add repository layer for contract function call

---
+++ src/main/kotlin/com/ampnet/blockchainapiservice/model/params/StoreContractFunctionCallRequestParams.kt
@@ -0,0 +1,28 @@
+package com.ampnet.blockchainapiservice.model.params
+
+import com.ampnet.blockchainapiservice.model.ScreenConfig
+import com.ampnet.blockchainapiservice.util.Balance
+import com.ampnet.blockchainapiservice.util.ChainId
+import com.ampnet.blockchainapiservice.util.ContractAddress
+import com.ampnet.blockchainapiservice.util.ContractBinaryData
+import com.ampnet.blockchainapiservice.util.UtcDateTime
+import com.ampnet.blockchainapiservice.util.WalletAddress
+import com.fasterxml.jackson.databind.JsonNode
+import java.util.UUID
+
+data class StoreContractFunctionCallRequestParams(
+    val id: UUID,
+    val deployedContractId: UUID?,
+    val contractAddress: ContractAddress,
+    val functionSignature: String,
+    val callData: ContractBinaryData,
+    val functionParams: JsonNode,
+    val ethAmount: Balance,
+    val chainId: ChainId,
+    val redirectUrl: String,
+    val projectId: UUID,
+    val createdAt: UtcDateTime,
+    val arbitraryData: JsonNode?,
+    val screenConfig: ScreenConfig,
+    val callerAddress: WalletAddress?
+)
