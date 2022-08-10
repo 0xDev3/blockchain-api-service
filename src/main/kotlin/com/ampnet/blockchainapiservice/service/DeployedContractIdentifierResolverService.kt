374cbd72ce71b78c5315a1064e21bf7b8754d440
Domagoj Latečki <domagoj.latecki@gmail.com>
2022-08-10 11:30:43 +0200

    SD-847: document error codes

--- src/main/kotlin/com/ampnet/blockchainapiservice/service/DeployedContractIdentifierResolverService.kt
+++ src/main/kotlin/com/ampnet/blockchainapiservice/service/DeployedContractIdentifierResolverService.kt
@@ -1,12 +1,13 @@
 package com.ampnet.blockchainapiservice.service
 
 import com.ampnet.blockchainapiservice.model.params.DeployedContractIdentifier
+import com.ampnet.blockchainapiservice.model.result.Project
 import com.ampnet.blockchainapiservice.util.ContractAddress
 import java.util.UUID
 
 interface DeployedContractIdentifierResolverService {
     fun resolveContractIdAndAddress(
         identifier: DeployedContractIdentifier,
-        projectId: UUID
+        project: Project
     ): Pair<UUID?, ContractAddress>
 }
