4b090a183bca32a8f60a4e4a1ab4c5429ee3dddc
Domagoj Latečki <domagoj.latecki@gmail.com>
2022-09-19 14:07:52 +0200

    SD-935: add fetching of contract decorators by project id

--- src/main/kotlin/com/ampnet/blockchainapiservice/controller/ContractDecoratorController.kt
+++ src/main/kotlin/com/ampnet/blockchainapiservice/controller/ContractDecoratorController.kt
@@ -12,6 +12,7 @@ import com.ampnet.blockchainapiservice.model.response.ContractDecoratorsResponse
 import com.ampnet.blockchainapiservice.model.response.InfoMarkdownsResponse
 import com.ampnet.blockchainapiservice.model.response.ManifestJsonsResponse
 import com.ampnet.blockchainapiservice.repository.ContractDecoratorRepository
+import com.ampnet.blockchainapiservice.repository.ImportedContractDecoratorRepository
 import com.ampnet.blockchainapiservice.util.ContractId
 import com.ampnet.blockchainapiservice.util.ContractTag
 import com.ampnet.blockchainapiservice.util.ContractTrait
@@ -22,92 +23,110 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 import javax.validation.Valid
 
 @Validated
 @RestController
-class ContractDecoratorController(private val contractDecoratorRepository: ContractDecoratorRepository) {
+class ContractDecoratorController(
+    private val contractDecoratorRepository: ContractDecoratorRepository,
+    private val importedContractDecoratorRepository: ImportedContractDecoratorRepository
+) {
 
     @GetMapping("/v1/deployable-contracts")
     fun getContractDecorators(
         @Valid @RequestParam("tags", required = false) contractTags: List<@MaxStringSize String>?,
-        @Valid @RequestParam("implements", required = false) contractImplements: List<@MaxStringSize String>?
+        @Valid @RequestParam("implements", required = false) contractImplements: List<@MaxStringSize String>?,
+        @RequestParam("projectId", required = false) projectId: UUID?
     ): ResponseEntity<ContractDecoratorsResponse> {
-        val contractDecorators = contractDecoratorRepository.getAll(
-            ContractDecoratorFilters(
-                contractTags = contractTags.parseOrListWithNestedAndLists { ContractTag(it) },
-                contractImplements = contractImplements.parseOrListWithNestedAndLists { ContractTrait(it) }
-            )
+        val filters = ContractDecoratorFilters(
+            contractTags = contractTags.parseOrListWithNestedAndLists { ContractTag(it) },
+            contractImplements = contractImplements.parseOrListWithNestedAndLists { ContractTrait(it) }
         )
+        val contractDecorators = contractDecoratorRepository.getAll(filters) +
+            projectId.getIfPresent { importedContractDecoratorRepository.getAll(it, filters) }
         return ResponseEntity.ok(ContractDecoratorsResponse(contractDecorators.map { ContractDecoratorResponse(it) }))
     }
 
     @GetMapping("/v1/deployable-contracts/manifest.json")
     fun getContractManifestJsonFiles(
         @Valid @RequestParam("tags", required = false) contractTags: List<@MaxStringSize String>?,
-        @Valid @RequestParam("implements", required = false) contractImplements: List<@MaxStringSize String>?
+        @Valid @RequestParam("implements", required = false) contractImplements: List<@MaxStringSize String>?,
+        @RequestParam("projectId", required = false) projectId: UUID?
     ): ResponseEntity<ManifestJsonsResponse> {
-        val contractManifests = contractDecoratorRepository.getAllManifestJsonFiles(
-            ContractDecoratorFilters(
-                contractTags = contractTags.parseOrListWithNestedAndLists { ContractTag(it) },
-                contractImplements = contractImplements.parseOrListWithNestedAndLists { ContractTrait(it) }
-            )
+        val filters = ContractDecoratorFilters(
+            contractTags = contractTags.parseOrListWithNestedAndLists { ContractTag(it) },
+            contractImplements = contractImplements.parseOrListWithNestedAndLists { ContractTrait(it) }
         )
+        val contractManifests = contractDecoratorRepository.getAllManifestJsonFiles(filters) +
+            projectId.getIfPresent { importedContractDecoratorRepository.getAllManifestJsonFiles(it, filters) }
         return ResponseEntity.ok(ManifestJsonsResponse(contractManifests))
     }
 
     @GetMapping("/v1/deployable-contracts/artifact.json")
     fun getContractArtifactJsonFiles(
         @Valid @RequestParam("tags", required = false) contractTags: List<@MaxStringSize String>?,
-        @Valid @RequestParam("implements", required = false) contractImplements: List<@MaxStringSize String>?
+        @Valid @RequestParam("implements", required = false) contractImplements: List<@MaxStringSize String>?,
+        @RequestParam("projectId", required = false) projectId: UUID?
     ): ResponseEntity<ArtifactJsonsResponse> {
-        val contractArtifacts = contractDecoratorRepository.getAllArtifactJsonFiles(
-            ContractDecoratorFilters(
-                contractTags = contractTags.parseOrListWithNestedAndLists { ContractTag(it) },
-                contractImplements = contractImplements.parseOrListWithNestedAndLists { ContractTrait(it) }
-            )
+        val filters = ContractDecoratorFilters(
+            contractTags = contractTags.parseOrListWithNestedAndLists { ContractTag(it) },
+            contractImplements = contractImplements.parseOrListWithNestedAndLists { ContractTrait(it) }
         )
+        val contractArtifacts = contractDecoratorRepository.getAllArtifactJsonFiles(filters) +
+            projectId.getIfPresent { importedContractDecoratorRepository.getAllArtifactJsonFiles(it, filters) }
         return ResponseEntity.ok(ArtifactJsonsResponse(contractArtifacts))
     }
 
     @GetMapping("/v1/deployable-contracts/info.md")
     fun getContractInfoMarkdownFiles(
         @Valid @RequestParam("tags", required = false) contractTags: List<@MaxStringSize String>?,
-        @Valid @RequestParam("implements", required = false) contractImplements: List<@MaxStringSize String>?
+        @Valid @RequestParam("implements", required = false) contractImplements: List<@MaxStringSize String>?,
+        @RequestParam("projectId", required = false) projectId: UUID?
     ): ResponseEntity<InfoMarkdownsResponse> {
-        val contractInfoMarkdowns = contractDecoratorRepository.getAllInfoMarkdownFiles(
-            ContractDecoratorFilters(
-                contractTags = contractTags.parseOrListWithNestedAndLists { ContractTag(it) },
-                contractImplements = contractImplements.parseOrListWithNestedAndLists { ContractTrait(it) }
-            )
+        val filters = ContractDecoratorFilters(
+            contractTags = contractTags.parseOrListWithNestedAndLists { ContractTag(it) },
+            contractImplements = contractImplements.parseOrListWithNestedAndLists { ContractTrait(it) }
         )
+        val contractInfoMarkdowns = contractDecoratorRepository.getAllInfoMarkdownFiles(filters) +
+            projectId.getIfPresent { importedContractDecoratorRepository.getAllInfoMarkdownFiles(it, filters) }
         return ResponseEntity.ok(InfoMarkdownsResponse(contractInfoMarkdowns))
     }
 
     @GetMapping("/v1/deployable-contracts/{id}")
     fun getContractDecorator(
-        @PathVariable("id") id: String
+        @PathVariable("id") id: String,
+        @RequestParam("projectId", required = false) projectId: UUID?
     ): ResponseEntity<ContractDecoratorResponse> {
-        val contractDecorator = contractDecoratorRepository.getById(ContractId(id)) ?: throw ResourceNotFoundException(
-            "Contract decorator not found for contract ID: $id"
-        )
+        val contractId = ContractId(id)
+        val contractDecorator = contractDecoratorRepository.getById(contractId)
+            ?: projectId?.let { importedContractDecoratorRepository.getByContractIdAndProjectId(contractId, it) }
+            ?: throw ResourceNotFoundException("Contract decorator not found for contract ID: $id")
         return ResponseEntity.ok(ContractDecoratorResponse(contractDecorator))
     }
 
     @GetMapping("/v1/deployable-contracts/{id}/manifest.json")
     fun getContractManifestJson(
-        @PathVariable("id") id: String
+        @PathVariable("id") id: String,
+        @RequestParam("projectId", required = false) projectId: UUID?
     ): ResponseEntity<ManifestJson> {
-        val manifestJson = contractDecoratorRepository.getManifestJsonById(ContractId(id))
+        val contractId = ContractId(id)
+        val manifestJson = contractDecoratorRepository.getManifestJsonById(contractId)
+            ?: projectId
+                ?.let { importedContractDecoratorRepository.getManifestJsonByContractIdAndProjectId(contractId, it) }
             ?: throw ResourceNotFoundException("Contract manifest.json not found for contract ID: $id")
         return ResponseEntity.ok(manifestJson)
     }
 
     @GetMapping("/v1/deployable-contracts/{id}/artifact.json")
     fun getContractArtifactJson(
-        @PathVariable("id") id: String
+        @PathVariable("id") id: String,
+        @RequestParam("projectId", required = false) projectId: UUID?
     ): ResponseEntity<ArtifactJson> {
-        val artifactJson = contractDecoratorRepository.getArtifactJsonById(ContractId(id))
+        val contractId = ContractId(id)
+        val artifactJson = contractDecoratorRepository.getArtifactJsonById(contractId)
+            ?: projectId
+                ?.let { importedContractDecoratorRepository.getArtifactJsonByContractIdAndProjectId(contractId, it) }
             ?: throw ResourceNotFoundException("Contract artifact.json not found for contract ID: $id")
         return ResponseEntity.ok(artifactJson)
     }
@@ -117,10 +136,16 @@ class ContractDecoratorController(private val contractDecoratorRepository: Contr
         produces = [MediaType.TEXT_MARKDOWN_VALUE]
     )
     fun getContractInfoMarkdown(
-        @PathVariable("id") id: String
+        @PathVariable("id") id: String,
+        @RequestParam("projectId", required = false) projectId: UUID?
     ): ResponseEntity<String> {
-        val infoMarkdown = contractDecoratorRepository.getInfoMarkdownById(ContractId(id))
+        val contractId = ContractId(id)
+        val infoMarkdown = contractDecoratorRepository.getInfoMarkdownById(contractId)
+            ?: projectId
+                ?.let { importedContractDecoratorRepository.getInfoMarkdownByContractIdAndProjectId(contractId, it) }
             ?: throw ResourceNotFoundException("Contract info.md not found for contract ID: $id")
         return ResponseEntity.ok(infoMarkdown)
     }
+
+    private fun <T> UUID?.getIfPresent(fn: (UUID) -> List<T>): List<T> = if (this != null) fn(this) else emptyList()
 }
---
+++ src/test/kotlin/com/ampnet/blockchainapiservice/controller/ImportContractControllerTest.kt
@@ -0,0 +1,170 @@
+package com.ampnet.blockchainapiservice.controller
+
+import com.ampnet.blockchainapiservice.JsonSchemaDocumentation
+import com.ampnet.blockchainapiservice.TestBase
+import com.ampnet.blockchainapiservice.TestData
+import com.ampnet.blockchainapiservice.model.ScreenConfig
+import com.ampnet.blockchainapiservice.model.params.ImportContractParams
+import com.ampnet.blockchainapiservice.model.request.ImportContractRequest
+import com.ampnet.blockchainapiservice.model.response.ContractDeploymentRequestResponse
+import com.ampnet.blockchainapiservice.model.response.TransactionResponse
+import com.ampnet.blockchainapiservice.model.result.ContractDeploymentRequest
+import com.ampnet.blockchainapiservice.model.result.Project
+import com.ampnet.blockchainapiservice.service.ContractDeploymentRequestService
+import com.ampnet.blockchainapiservice.service.ContractImportService
+import com.ampnet.blockchainapiservice.util.Balance
+import com.ampnet.blockchainapiservice.util.BaseUrl
+import com.ampnet.blockchainapiservice.util.ChainId
+import com.ampnet.blockchainapiservice.util.ContractAddress
+import com.ampnet.blockchainapiservice.util.ContractBinaryData
+import com.ampnet.blockchainapiservice.util.ContractId
+import com.ampnet.blockchainapiservice.util.ContractTag
+import com.ampnet.blockchainapiservice.util.ContractTrait
+import com.ampnet.blockchainapiservice.util.FunctionData
+import com.ampnet.blockchainapiservice.util.Status
+import com.ampnet.blockchainapiservice.util.TransactionData
+import com.ampnet.blockchainapiservice.util.TransactionHash
+import com.ampnet.blockchainapiservice.util.WalletAddress
+import com.ampnet.blockchainapiservice.util.WithTransactionData
+import com.ampnet.blockchainapiservice.util.ZeroAddress
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.given
+import org.mockito.kotlin.mock
+import org.springframework.http.ResponseEntity
+import java.math.BigInteger
+import java.util.UUID
+
+class ImportContractControllerTest : TestBase() {
+
+    @Test
+    fun mustCorrectlyImportContract() {
+        val txHash = TransactionHash("tx-hash")
+        val result = WithTransactionData(
+            value = ContractDeploymentRequest(
+                id = UUID.randomUUID(),
+                alias = "alias",
+                name = "name",
+                description = "description",
+                contractId = ContractId("contract-id"),
+                contractData = ContractBinaryData("00"),
+                constructorParams = TestData.EMPTY_JSON_ARRAY,
+                contractTags = listOf(ContractTag("contract-tag")),
+                contractImplements = listOf(ContractTrait("contract-trait")),
+                initialEthAmount = Balance(BigInteger.TEN),
+                chainId = ChainId(1337),
+                redirectUrl = "redirect-url",
+                projectId = UUID.randomUUID(),
+                createdAt = TestData.TIMESTAMP,
+                arbitraryData = TestData.EMPTY_JSON_OBJECT,
+                screenConfig = ScreenConfig(
+                    beforeActionMessage = "before-action-message",
+                    afterActionMessage = "after-action-message"
+                ),
+                contractAddress = ContractAddress("cafebabe"),
+                deployerAddress = WalletAddress("a"),
+                txHash = txHash,
+                imported = false
+            ),
+            status = Status.SUCCESS,
+            transactionData = TransactionData(
+                txHash = txHash,
+                fromAddress = WalletAddress("b"),
+                toAddress = ZeroAddress,
+                data = FunctionData("00"),
+                value = Balance(BigInteger.TEN),
+                blockConfirmations = BigInteger.ONE,
+                timestamp = TestData.TIMESTAMP
+            )
+        )
+
+        val importService = mock<ContractImportService>()
+        val params = ImportContractParams(
+            alias = result.value.alias,
+            contractId = result.value.contractId,
+            contractAddress = result.value.contractAddress!!,
+            redirectUrl = result.value.redirectUrl,
+            arbitraryData = result.value.arbitraryData,
+            screenConfig = result.value.screenConfig
+        )
+        val projectId = UUID.randomUUID()
+        val project = Project(
+            id = projectId,
+            ownerId = UUID.randomUUID(),
+            issuerContractAddress = ContractAddress("abc123"),
+            baseRedirectUrl = BaseUrl("base-url"),
+            chainId = ChainId(1337L),
+            customRpcUrl = null,
+            createdAt = TestData.TIMESTAMP
+        )
+
+        suppose("some smart contract will be imported") {
+            given(importService.importContract(params, project))
+                .willReturn(result.value.id)
+        }
+
+        val deploymentService = mock<ContractDeploymentRequestService>()
+
+        suppose("imported contract will be deployed") {
+            given(deploymentService.getContractDeploymentRequest(result.value.id))
+                .willReturn(result)
+        }
+
+        val controller = ImportContractController(importService, deploymentService)
+
+        verify("controller returns correct response") {
+            val request = ImportContractRequest(
+                alias = result.value.alias,
+                contractId = result.value.contractId.value,
+                contractAddress = result.value.contractAddress!!.rawValue,
+                redirectUrl = result.value.redirectUrl,
+                arbitraryData = result.value.arbitraryData,
+                screenConfig = result.value.screenConfig
+            )
+            val response = controller.importSmartContract(
+                project = project,
+                requestBody = request
+            )
+
+            JsonSchemaDocumentation.createSchema(request.javaClass)
+            JsonSchemaDocumentation.createSchema(response.body!!.javaClass)
+
+            assertThat(response).withMessage()
+                .isEqualTo(
+                    ResponseEntity.ok(
+                        ContractDeploymentRequestResponse(
+                            id = result.value.id,
+                            alias = result.value.alias,
+                            name = result.value.name,
+                            description = result.value.description,
+                            status = result.status,
+                            contractId = result.value.contractId.value,
+                            contractDeploymentData = result.value.contractData.withPrefix,
+                            constructorParams = TestData.EMPTY_JSON_ARRAY,
+                            contractTags = result.value.contractTags.map { it.value },
+                            contractImplements = result.value.contractImplements.map { it.value },
+                            initialEthAmount = result.value.initialEthAmount.rawValue,
+                            chainId = result.value.chainId.value,
+                            redirectUrl = result.value.redirectUrl,
+                            projectId = result.value.projectId,
+                            createdAt = result.value.createdAt.value,
+                            arbitraryData = result.value.arbitraryData,
+                            screenConfig = result.value.screenConfig.orEmpty(),
+                            contractAddress = result.value.contractAddress?.rawValue,
+                            deployerAddress = result.value.deployerAddress?.rawValue,
+                            deployTx = TransactionResponse(
+                                txHash = result.transactionData.txHash?.value,
+                                from = result.transactionData.fromAddress?.rawValue,
+                                to = result.transactionData.toAddress.rawValue,
+                                data = result.transactionData.data?.value,
+                                value = result.transactionData.value.rawValue,
+                                blockConfirmations = result.transactionData.blockConfirmations,
+                                timestamp = result.transactionData.timestamp?.value
+                            ),
+                            imported = result.value.imported
+                        )
+                    )
+                )
+        }
+    }
+}
