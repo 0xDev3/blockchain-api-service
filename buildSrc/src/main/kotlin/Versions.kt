448dbd5cbc76a6036d3de014da09a35ae37c2cc4
Domagoj Latečki <domagoj.latecki@gmail.com>
2022-10-12 16:29:08 +0200

    CORS fix

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 
 object Versions {
 
-    const val project = "1.2.1"
+    const val project = "1.2.2"
 
     object Compile {
         const val kotlin = "1.6.10"
