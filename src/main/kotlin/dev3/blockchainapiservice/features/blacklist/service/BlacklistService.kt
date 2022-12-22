0fdc1b1bbf0324e56377f6b69c18cc171510ae6f
Domagoj Latečki <domagoj.latecki@gmail.com>
2022-12-22 11:59:12 +0100

    dev3-46: add blacklist service and controller

---
+++ src/main/kotlin/dev3/blockchainapiservice/features/blacklist/service/BlacklistService.kt
@@ -0,0 +1,11 @@
+package dev3.blockchainapiservice.features.blacklist.service
+
+import dev3.blockchainapiservice.model.result.UserIdentifier
+import dev3.blockchainapiservice.util.EthereumAddress
+import dev3.blockchainapiservice.util.WalletAddress
+
+interface BlacklistService {
+    fun addAddress(userIdentifier: UserIdentifier, address: EthereumAddress)
+    fun removeAddress(userIdentifier: UserIdentifier, address: EthereumAddress)
+    fun listAddresses(userIdentifier: UserIdentifier): List<WalletAddress>
+}
---
+++ src/test/kotlin/dev3/blockchainapiservice/features/blacklist/controller/BlacklistControllerTest.kt
@@ -0,0 +1,79 @@
+package dev3.blockchainapiservice.features.blacklist.controller
+
+import dev3.blockchainapiservice.JsonSchemaDocumentation
+import dev3.blockchainapiservice.TestBase
+import dev3.blockchainapiservice.features.blacklist.model.response.BlacklistedAddressesResponse
+import dev3.blockchainapiservice.features.blacklist.service.BlacklistService
+import dev3.blockchainapiservice.model.result.UserWalletAddressIdentifier
+import dev3.blockchainapiservice.util.WalletAddress
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.springframework.http.ResponseEntity
+import java.util.UUID
+
+class BlacklistControllerTest : TestBase() {
+
+    companion object {
+        private val USER_IDENTIFIER = UserWalletAddressIdentifier(
+            id = UUID.randomUUID(),
+            stripeClientId = null,
+            walletAddress = WalletAddress("abc")
+        )
+        private val ADDRESS = WalletAddress("cafebabe")
+    }
+
+    @Test
+    fun mustCorrectlyAddAddressToBlacklist() {
+        val service = mock<BlacklistService>()
+        val controller = BlacklistController(service)
+
+        verify("service is correctly called") {
+            controller.addAddress(USER_IDENTIFIER, ADDRESS.rawValue)
+
+            expectInteractions(service) {
+                once.addAddress(USER_IDENTIFIER, ADDRESS)
+            }
+        }
+    }
+
+    @Test
+    fun mustCorrectlyRemoveAddressFromBlacklist() {
+        val service = mock<BlacklistService>()
+        val controller = BlacklistController(service)
+
+        verify("service is correctly called") {
+            controller.removeAddress(USER_IDENTIFIER, ADDRESS.rawValue)
+
+            expectInteractions(service) {
+                once.removeAddress(USER_IDENTIFIER, ADDRESS)
+            }
+        }
+    }
+
+    @Test
+    fun mustCorrectlyListBlacklistedAddresses() {
+        val service = mock<BlacklistService>()
+
+        suppose("some blacklisted addresses will be returned") {
+            call(service.listAddresses(USER_IDENTIFIER))
+                .willReturn(listOf(ADDRESS))
+        }
+
+        val controller = BlacklistController(service)
+
+        verify("controller returns correct response") {
+            val response = controller.listAddresses(USER_IDENTIFIER)
+
+            JsonSchemaDocumentation.createSchema(response.body!!.javaClass)
+
+            expectThat(response)
+                .isEqualTo(
+                    ResponseEntity.ok(
+                        BlacklistedAddressesResponse(
+                            listOf(ADDRESS.rawValue)
+                        )
+                    )
+                )
+        }
+    }
+}
